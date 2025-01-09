cb336c57f791994519bffe6c1fd2d83896e036d1
Ricardo Ferreira <ricardoferreiar@gmail.com>
2025-01-09 22:22:43 +0000

    Create casino.sh
    Função do menu inicial

---
+++ casino.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Função para exibir o menu inicial
+function menu() {
+  echo "========================"
+  echo "🎰 Bem-vindo ao Cassino 🎲"
+  echo "========================"
+  echo "1. Roleta"
+  echo "2. slot machine"
+  echo "3. guess the number"
+  echo "4. Sair"
+  echo "Escolha um jogo: "
+}
